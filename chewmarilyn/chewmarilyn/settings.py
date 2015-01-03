2d5c1d5a82dedf9a9eb36bbaefaea61f6b64e728
Ng Zhi An <ngzhian@gmail.com>
2015-01-03 21:02:35 +0000

    add static root

--- chewmarilyn/chewmarilyn/settings.py
+++ chewmarilyn/chewmarilyn/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/var/www/public/static/'
+STATIC_ROOT = '/var/www/public/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
